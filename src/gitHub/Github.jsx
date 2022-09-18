8a2f48ef42bc424e9104f89e239686efd8bfd684
Utkarsh <famedustevershine@gmail.com>
2022-09-18 19:51:34 +0530

    Done

--- src/gitHub/Github.jsx
+++ src/gitHub/Github.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./github.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 import github from "../Assets/Home/github.png"
 
 const Github = () => {
+  AOS.init({ duration:2000})
   return (
-    <div className='main_git'>
+    <div className='main_git' data-aos="flip-down">
       <div className='child'>
             <img src={github} alt="" />
       </div>
