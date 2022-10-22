0ed2e41dee311b452e92c6f8cddaa2915d8c3206
Utkarsh <famedustevershine@gmail.com>
2022-10-22 21:03:07 +0530

    Done

--- src/PortfolioContent/Home/Navigation/Navogation.jsx
+++ src/PortfolioContent/Home/Navigation/Navogation.jsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
 // import AppScroll from '../hr/HR'
 import "./Nav.css";
 import { HashLink as Link} from 'react-router-hash-link';
 import { useEffect } from 'react';
 
 const Navigation = () => {
+  const [navbar, setNavbar] = useState(false);
 
-
-
+  const changeBg = ()=>{
+      if(window.scrollY>=50){
+          setNavbar(true)
+      }else{
+          setNavbar(false)
+      }
+  }
+  window.addEventListener("scroll",changeBg)
   return (
     <div className='nav-container'>
-        <div className='nav-parent'>
+        <div className={navbar?"navbar-parent scroll":"navbar-parent"} >
             <div className='nav-icon'>
             <div className="waviy">
                 <span classsName="--i:1" >U</span>
@@ -28,6 +35,7 @@ const Navigation = () => {
                 <li><Link smooth to="#skills" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}>Skills</Link></li>
                 <li><Link smooth to="#projects" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}>Projects</Link></li>
                 <li><Link smooth to="#contact" scroll={(el) => el.scrollIntoView({ behavior: 'auto', block: 'end' })}>Contact</Link></li>
+                <li><a href="Utkarshfw19_Resume.pdf" download="Utkarsh_Resume">Resume</a></li>
             </div>
         </div>
     </div>
